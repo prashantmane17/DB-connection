176dc773b1c640de880447fff830141e895a1500
prashantmane17 <prashantmn47@gmail.com>
2024-10-07 17:13:35 +0530

    spaces removed

--- src/helper/db.js
+++ src/helper/db.js
@@ -1,19 +1,13 @@
-import { User } from "../models/user";
 import mongoose from "mongoose";
 export const connectDb = async () => {
-
     try {
         const { connection } = await mongoose.connect(process.env.MONGO_DB_URL, {
             dbName: 'work_manager',
-
         });
         console.log("db connect...");
-
-      
         console.log("con " + connection.host)
     }
     catch (error) {
         console.l; og("failed connection")
     }
-
 }
